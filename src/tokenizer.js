66b87a0ed0a7d4398dc24ae82113e299afeb91ce
bergsans <bergclaesmagnus@gmail.com>
2020-09-10 12:43:48 +0200

    add throws, plus, ws and refactor

--- src/tokenizer.js
+++ src/tokenizer.js
@@ -6,33 +6,65 @@ class TokenError extends Error {
   }
 }
 
-const isDigit = (char) => /[0-9]/.test(char);
+const isEOF = (char) => char === undefined
+
+const tokenChecks = {
+  isDigit: (char) => /[0-9]/.test(char),
+  isAddition: (char) => /\+/.test(char),
+  isWhiteSpace: (char) => / /.test(char),
+  isLeftParen: (char) => /\(/.test(char),
+  isRightParen: (char) => /\)/.test(char),
+  isAlphabetic: (char) => /[A-Za-z]/.test(char)
+};
+
+const TOKEN_TYPES = {
+  isDigit: 'INTEGER',
+  isAddition: 'PLUS',
+  isWhiteSpace: 'WHITE_SPACE',
+  isLeftParen: 'OPEN_PAREN',
+  isRightParen: 'CLOSE_PAREN',
+  isAlphabetic: 'IDENTIFIER'
+};
+
+const removeWhiteSpace = (token) => token.type !== 'WHITE_SPACE';
+
+function collectCharacters(fn, input, currentPosition) {
+  let tempValue = input[currentPosition];
+  while(fn(input[currentPosition + 1])) {
+    currentPosition++;
+    tempValue += input[currentPosition];
+  }
+  console.log(tempValue);
+  return tempValue;
+}
 
 function tokenize(input, currentPosition = 0, tokens = []) {
-  if(currentPosition === input.length) {
-    return [...tokens, { type: 'EOF' }];
-  } else if(isDigit(input[currentPosition])) {
-    if(isDigit(input[currentPosition + 1])) {
-      let tempInt = input[currentPosition];
-      while(isDigit(input[currentPosition + 1])) {
-        currentPosition++;
-        tempInt += input[currentPosition];
-      }
-      return tokenize(
-        input,
-        currentPosition + 1,
-        [...tokens, { type: 'INTEGER', value: tempInt }]
-      );
-    } else {
-      return tokenize(
-        input,
-        currentPosition + 1,
-        [...tokens, { type: 'INTEGER', value: input[currentPosition] }]
-      );
-    }
-  } else {
-    throw new TokenError(input[currentPosition]);
+  if(isEOF(input[currentPosition])) {
+    return [...tokens, { type: 'EOF' }].filter(removeWhiteSpace);
   }
+
+  const token = Object
+    .entries(tokenChecks)
+    .find(([_name, fn]) => fn(input[currentPosition]));
+  if(token) {
+   const [name, fn] = token;
+   return tokenize(
+     input,
+     name === 'isDigit'
+      ? currentPosition + collectCharacters(fn, input, currentPosition).length
+      : currentPosition + 1,
+    [
+       ...tokens,
+       {
+         type: TOKEN_TYPES[name],
+         value: name === 'isDigit'
+          ? collectCharacters(fn, input, currentPosition)
+          : input[currentPosition],
+       }
+    ]
+   );
+  }
+  throw new TokenError(input[currentPosition]);
 }
 
 module.exports = {
