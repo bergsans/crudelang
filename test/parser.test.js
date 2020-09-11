c6378c1b5383f0b67ec91e41ad44faac9a0bd25d
bergsans <bergclaesmagnus@gmail.com>
2020-09-11 09:08:53 +0200

    Make simple parsing tests

--- test/parser.test.js
+++ test/parser.test.js
@@ -2,11 +2,52 @@ const { tokenize } = require('../src/tokenizer.js');
 const { parse } = require('../src/parser.js');
 
 test('parse 1', () => {
-  expect(parse(tokenize('1'))).toEqual({});
+  expect(parse(tokenize('1'))).toEqual({
+    type: 'INTEGER',
+    value: '1'
+  });
 });
+
 test('parse 1 + 1', () => {
-  expect(parse(tokenize('1 + 1'))).toEqual({});
+  expect(parse(tokenize('1 + 1'))).toEqual({
+    left: {
+      type: 'INTEGER',
+      value: '1'
+    },
+    op: {
+      type: 'PLUS',
+      value: '+'
+    },
+    right: {
+      type: 'INTEGER',
+      value: '1'
+    }
+  });
 });
+
 test('parse 1 + 1 + 1', () => {
-  expect(parse(tokenize('1 + 1 + 1'))).toEqual({});
+  expect(parse(tokenize('1 + 1 + 1'))).toEqual({
+    left: {
+      type: 'INTEGER',
+      value: '1'
+    },
+    op: {
+      type: 'PLUS',
+      value: '+'
+    },
+    right: {
+      left: {
+        type: 'INTEGER',
+        value: '1'
+      },
+      op: {
+        type: 'PLUS',
+        value: '+'
+      },
+      right: {
+        type: 'INTEGER',
+        value: '1'
+      }
+    }
+  });
 });
